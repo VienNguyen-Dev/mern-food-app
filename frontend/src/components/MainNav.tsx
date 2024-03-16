c6d36370b0d20d61f0536eaeff358b83eb9c244d
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-03-16 11:48:09 +0700

    Auth setup

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,8 +1,10 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 export default function MainNav() {
+  const { loginWithRedirect } = useAuth0();
   return (
-    <Button variant={"ghost"} className="text-lg font-bold hover:bg-white hover:text-orange-500">
+    <Button onClick={async () => await loginWithRedirect()} variant={"ghost"} className="text-lg font-bold hover:bg-white hover:text-orange-500">
       Log In
     </Button>
   );
